4945da3c90bd8d036675ef8657a06d219a79d77d
weixiao122 <42261449+weixiao122@users.noreply.github.com>
2019-09-12 11:05:57 +0800

    Update CreateExcel.py

--- 1-CreateExcel/CreateExcel.py
+++ 1-CreateExcel/CreateExcel.py
@@ -5,14 +5,18 @@
 
 import pandas as pd
 
+#设置写入文件
+writer = pd.ExcelWriter('output.xlsx')
+
 df = pd.DataFrame({'ID':[0,1,2],'Name':['Mark','Tomi','Jack']})
 
 # 此处设置数据表的索引，如未设置索引会自动在最前方添加一列作为索引
 df = df.set_index('ID') # 会生成新的 DataFrame
 # df.set_index('ID',inplace=True) # 在原来的 DataFrame 上进行修改
 
-# 将数据报错到 output.xlsx
-df.to_excel('./output.xlsx')
+# 将数据输出到 output.xlsx
+df.to_excel(writer,'Sheet1')
+writer.save()
 
 print(df)
 print('Done')
