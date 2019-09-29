8dc834bccd723860633e4882d41f13d45a9029e4
weixiao122 <42261449+weixiao122@users.noreply.github.com>
2019-09-29 11:44:20 +0800

    Update ReadDataBase.py

--- 29-ReadDataBase/ReadDataBase.py
+++ 29-ReadDataBase/ReadDataBase.py
@@ -3,6 +3,8 @@
 # 第二十九课 读取数据库
 # 建议在第八讲之后查看
 # 2018-10-30
+#安装sqlalchemy模块,pycharm可以在setting里面安装,ide可以使用pip install sqlalchemy来安装
+#安装pymysql模块,可以连接mysql,pycharm可以在setting里面安装,ide可以使用pip install pymysql来安装
 
 import pyodbc
 import sqlalchemy
@@ -13,6 +15,8 @@ import pandas as pd
 connection = pyodbc.connect('DRIVER={SQL Server}; SERVER=(local); DATABASE=AdventureWorks;USER=sa;PASSWORD=123456')
 # sqlalchemy 链接字符串
 engine = sqlalchemy.create_engine('mssql+pyodbc://sa:123456@(local)/AdventureWorks?driver=SQL+Server')
+#engine=create_engine("mysql+pymysql://root:a5230411@localhost:3306/test",echo=True)    #连接mysql
+
 
 # 由于数据库中使用单引号，此处使用双引号引用 SQL 语句
 query = "SELECT FirstName, LastName FROM Person.Person"
